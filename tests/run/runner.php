9dc9dc114b45a9ab96ea4811b9fbcbdc49137b33
bulton-fr <bulton.fr@gmail.com>
2018-08-17 22:14:24 +0200

    Add travis file and functional test
    Unit tests will be add later

---
+++ tests/run/runner.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once(__DIR__.'/../../vendor/autoload.php');
+
+$expected = file_get_contents(__DIR__.'/expected.txt');
+
+
+ob_start();
+$project = new \bultonFr\DocStructGenerator\ProjectParser(
+    __DIR__.'/../../vendor',
+    'bultonFr\\'
+);
+$project->run();
+
+$result = ob_get_contents();
+ob_end_clean();
+
+if ($result === $expected) {
+    echo "\033[1;37;42mResult returned corresponding to expected\033[0m\n";
+    exit(0);
+} else {
+    echo "\033[1;37;41mResult returned not corresponding to expected\033[0m\n";
+    exit(1);
+}
