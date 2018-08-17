6e4bf7e4c491077bbec86f0d8fc5467af3038191
bulton-fr <bulton.fr@gmail.com>
2018-08-17 22:31:36 +0200

    Run test : Add debug when text does not match

--- tests/run/runner.php
+++ tests/run/runner.php
@@ -19,6 +19,15 @@ if ($result === $expected) {
     echo "\033[1;37;42mResult returned corresponding to expected\033[0m\n";
     exit(0);
 } else {
-    echo "\033[1;37;41mResult returned not corresponding to expected\033[0m\n";
+    echo "\033[1;37;41mResult returned not corresponding to expected\033[0m\n\n";
+    echo 
+        'Expected Return ('.strlen($expected).'): '."\n"
+        .$expected
+        ."\n----------------------------\n"
+        .'Obtained Return ('.strlen($result).'): '."\n"
+        .$result
+        ."\n----------------------------\n"
+    ;
+    
     exit(1);
 }
